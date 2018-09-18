b1342796b78aca6adef1f9a7b587a940528fc1fc
Karanjot Singh <Karanjot.Singh@prodigi.local>
2018-09-18 15:23:09 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import Search from './components/Search'
 import './App.css';
 import './components/Search.css'
-import {Route} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
 import CheckUp from './components/CheckUp'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Switch>
         <Route exact path='/' render={() =>(<Search/>)} />
         <Route exact path='/checkup' render={() =>(<CheckUp/>)} />
+      </Switch>
       </div>
     );
   }
