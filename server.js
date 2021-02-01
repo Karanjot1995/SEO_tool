bc068cf607514afb17403d009e9a1a9cc7613704
Karanjot Singh <karan.nanda97@gmail.com>
2021-02-01 13:26:02 +0530

    push

--- server.js
+++ server.js
@@ -60,17 +60,17 @@ var request = require('request');
 //           function() {
 //             console.log(page.content())
 //             return page.content();
-            
+
 //           }),200
 //         ) 
 //       })
 //       .then(html => {
 //         const $ = cheerio.load(html);
-        
+
 
 //           res.send(html)
 //         })
-       
+
 
 // })
 
@@ -86,38 +86,38 @@ var request = require('request');
 //     },
 //     proxyList : proxyList
 //   };
-   
+
 //   const numberOfResults = await serp.search(options);
 //   console.log(numberOfResults)
 // }
 
- 
 
 
 
 
-app.get('/api/indexing', (req,res)=>{
+
+app.get('/api/indexing', (req, res) => {
   var url_parts = url.parse(req.url, true);
-    var query = url_parts.query;
-    puppeteer
-      .launch()
-      .then(browser => browser.newPage())
-      .then(page => {
-        return page.goto('https://www.google.com/search?q=site:'+query.url).then(function() {
-          console.log(page.content())
-          return page.content();
-          
-        });
-      })
-      .then(html => {
-        const $ = cheerio.load(html);
-        
+  var query = url_parts.query;
+  puppeteer
+    .launch()
+    .then(browser => browser.newPage())
+    .then(page => {
+      return page.goto('https://www.google.com/search?q=site:' + query.url).then(function () {
+        console.log(page.content())
+        return page.content();
 
-          res.send({html:html})
-        })
-       
+      });
+    })
+    .then(html => {
+      const $ = cheerio.load(html);
 
-      })
+
+      res.send({ html: html })
+    })
+
+
+})
 
 
 // app.get('/api/indexing', (req,res)=>{
@@ -153,68 +153,68 @@ app.get('/api/indexing', (req,res)=>{
 
 
 
-var urls =[];
-app.get('/api/cache', (req,res)=>{
-  
+var urls = [];
+app.get('/api/cache', (req, res) => {
+
   var url_parts = url.parse(req.url, true);
 
-  var a ="24"
+  var a = "24"
   var query = url_parts.query;
-  axios.get("https://webcache.googleusercontent.com/search?q=cache:"  + query.url)
+  axios.get("https://webcache.googleusercontent.com/search?q=cache:" + query.url)
     // console.log(response.status)
     .then(response => {
       response
-      res.send({response:(response.data)})
+      res.send({ response: (response.data) })
     })
 
 })
 
-app.get('/api/goo',(req,res)=>{
+app.get('/api/goo', (req, res) => {
   var url_parts = url.parse(req.url, true);
   var query = url_parts.query;
-  console.log('abcd',query.url);
+  console.log('abcd', query.url);
 
 
-  
-  axios.get("https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url="  + query.url)
+
+  axios.get("https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=" + query.url)
     // console.log(response.status)
     .then(response => {
       response
-      res.send({response:(response.data)})
+      res.send({ response: (response.data) })
       // console.log(response.data.audits.metri)
     })
-   
-    
-  })
-  
-function errBack (cb,THIS,logger) {
 
-  var 
-  self,
-  EB=function(fn,r,e){
-      if (logger===false) {
-          fn.log=fn.info=fn.warn=fn.errlog=function(){};       
+
+})
+
+function errBack(cb, THIS, logger) {
+
+  var
+    self,
+    EB = function (fn, r, e) {
+      if (logger === false) {
+        fn.log = fn.info = fn.warn = fn.errlog = function () { };
       } else {
-          fn.log        = logger?logger.log   : console.log.bind(console);
-          fn.info       = logger?logger.info  : console.info.bind(console);
-          fn.warn       = logger?logger.warn  : console.warn.bind(console);
-          fn.errlog     = logger?logger.error : console.error.bind(console);
+        fn.log = logger ? logger.log : console.log.bind(console);
+        fn.info = logger ? logger.info : console.info.bind(console);
+        fn.warn = logger ? logger.warn : console.warn.bind(console);
+        fn.errlog = logger ? logger.error : console.error.bind(console);
       }
-      fn.result=r;
-      fn.error=e;
-      return (self=fn);
-  };
+      fn.result = r;
+      fn.error = e;
+      return (self = fn);
+    };
 
 
-      return EB(
+  return EB(
 
-          function () {
-              return cb.apply (THIS,Array.prototype.concat.apply([undefined],arguments));
-          },
-          
-      );
+    function () {
+      return cb.apply(THIS, Array.prototype.concat.apply([undefined], arguments));
+    },
+
+  );
 }
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
@@ -224,35 +224,35 @@ app.get('/api/hello', (req, res) => {
 
   var url_parts = url.parse(req.url, true);
   var query = url_parts.query;
-  console.log('abcd',query.url);
+  console.log('abcd', query.url);
 
 
   var statuscodes = []
 
- 
 
- 
- var TLD_url = query.url;
- 
- https.get(TLD_url, function(res2){
-  //     var body = [];
-      var callback=errBack(res2);
-      
-      callback.info({statusCode:res2.statusCode});
-      statuscodes.push(res2.statusCode)
-})
 
-axios.get(query.url)
-  .then(response => {    
-      // response.data
-      console.log('my status' ,statuscodes)
-      res.send({test:response.data, statuscodes:statuscodes})
-    
+
+  var TLD_url = query.url;
+
+  https.get(TLD_url, function (res2) {
+    //     var body = [];
+    var callback = errBack(res2);
+
+    callback.info({ statusCode: res2.statusCode });
+    statuscodes.push(res2.statusCode)
   })
 
-  
+  axios.get(query.url)
+    .then(response => {
+      // response.data
+      console.log('my status', statuscodes)
+      res.send({ test: response.data, statuscodes: statuscodes })
 
-  
+    })
+
+
+
+
 });
 
 
